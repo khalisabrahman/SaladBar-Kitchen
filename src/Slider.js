627a0a51bbfc35c1a7247d90dae63ab1c2765728
Khalis Ab Rahman <khalisabrahman@gmail.com>
2020-11-26 20:56:26 +0800

    Edited the Slider component and added more styles

--- src/Slider.js
+++ src/Slider.js
@@ -1,48 +1,69 @@
 import React, {useState} from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import ImageComponent from './ImageComponent';
-import sliderPic1 from './img/sliderPics/sliderPic1.jpg'
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import sliderPic1 from './img/sliderPics/sliderPic1.jpg';
+import sliderPic2 from './img/sliderPics/sliderPic2.jpg'
 
 const useStyles = makeStyles((theme) => ({
 	slider: {
-		border: '1px solid red',
 		position: 'relative',
 		width: '100%',
-		height: '545px',
+		height: '100vh',
 		boxSizing: 'border-box',
 		margin: 0,
 		padding: 0,
 		display: 'flex',
-		alignItems: 'center',
+        alignItems: 'center',
+        overflow: 'hidden',
+        background: 'black'
 	},
 	slide: {
-		border: '1px solid blue',
+        position: 'relative',
 		minWidth: '100%',
-        height: '100%',
-        transition: '0.5s'
+        height: '80%',
+        transition: '0.5s',
+        overflow: 'hidden'
 	},
 	goLeft: {
 		position: 'absolute',
 		top: '50%',
 		left: 0,
-		transform: 'translateY(-50%)',
+        transform: 'translateY(-50%)',
+        transition: '0.5s',
 		width: '9%',
 		height: '100%',
-		background: 'none',
+        background: 'none',
+        border: 'none',
+        outline: 'none',
+        '&:hover': {
+            background: 'rgba(0,0,0,0.356)',
+            cursor: 'pointer',
+            color: 'whitesmoke'
+		}
 	},
 	goRight: {
 		position: 'absolute',
 		top: '50%',
 		right: 0,
-		transform: 'translateY(-50%)',
+        transform: 'translateY(-50%)',
+        transition: '0.5s',
 		width: '9%',
 		height: '100%',
-		background: 'none',
+        background: 'none',
+        border: 'none',
+        outline: 'none',
+        '&:hover': {
+            background: 'rgba(0,0,0,0.356)',
+            cursor: 'pointer',
+            color: 'whitesmoke'
+		}
 	},
 }));
 
 function Slider() {
-    let sliderArr = [<ImageComponent src={sliderPic1} />, 2, 3, 4, 5];
+    let sliderArr = [<ImageComponent src={sliderPic1} />, <ImageComponent src={sliderPic2} />, 3, 4, 5];
     const [x, setX] = useState(0);
     const buttonLeft = () => {
         x === 0 ? setX(-100 * (sliderArr.length -1)) : setX(x + 100)
@@ -63,10 +84,10 @@ function Slider() {
 				);
 			})}
 			<button className={classes.goLeft} onClick={buttonLeft}>
-				Left
+				<ChevronLeftIcon fontSize='large'/>
 			</button>
 			<button className={classes.goRight} onClick={buttonRight}>
-				Right
+				<ChevronRightIcon fontSize='large' />
 			</button>
 		</div>
 	);
